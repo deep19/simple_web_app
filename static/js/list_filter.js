fff2e469960510539be7ea197c7f80bf004d9764
deep19 <deeptanshu.choudhary@hindustantimes.com>
2016-03-01 16:04:58 +0530

    Testing google map working 8

--- static/js/list_filter.js
+++ static/js/list_filter.js
@@ -5,13 +5,24 @@ var options = {
 	getValue: "name",
 	list: {
 		onClickEvent: function() {
-			var value = $("#basics").getSelectedItemData().locations;
-            console.log(value)
+            onselect_handler()
 		},
 		onKeyEnterEvent: function() {
-			var value = $("#basics").getSelectedItemData().locations;
-            console.log(value)
+            onselect_handler()
 		}
 	},
 	requestDelay: 500
 };
+
+
+var onselect_handler = function(){
+    var value = $("#basics").getSelectedItemData().locations;
+    value = value[0]
+    console.log(value)
+    var google_map_radar_api_url = "https://maps.googleapis.com/maps/api/place/radarsearch/json?key=AIzaSyD1LmAPLKwVX9wLtB9sAoUrR6MTgCbAqoU&location=37.773972,-122.431297&radius=50000&name="+value
+    $.getJSON( google_map_radar_api_url, function( data ) {
+        console.log(data)
+    });
+}
+
+
