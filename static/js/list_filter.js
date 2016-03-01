dc41fd1814cf1ca872c4cff9e398355e7b881714
deep19 <deeptanshu.choudhary@hindustantimes.com>
2016-03-01 17:56:44 +0530

    Testing google map working 16(

--- static/js/list_filter.js
+++ static/js/list_filter.js
@@ -14,33 +14,50 @@ var options = {
 	requestDelay: 500
 };
 
+var markers = []
+
 
 var onselect_handler = function(){
-    var value = $("#basics").getSelectedItemData().locations;
-    value = value[0]
-    console.log(value)
-    var request = {
-        location:{lat:37.773972, lng: -122.431297},
-        radius:50000,
-        name: value+ " San Francisco"
-    }
-
-    var service = new google.maps.places.PlacesService(map);
-    service.radarSearch(request, 
-        function(results, status)
-        {
-            if (status == google.maps.places.PlacesServiceStatus.OK) 
-            {
-                var lat = (results[0].geometry.location.lat())
-                var lng = (results[0].geometry.location.lng())
-                var marker = new google.maps.Marker({
-                    position: {lat: lat, lng:lng},
-                    title: value
-                });
-                marker.setMap(map);
-            };
-
+    var locations = $("#basics").getSelectedItemData().locations;
+    console.log(locations)
+    locations.pop()
+    if (locations[0]){
+        var requests = locations.map(function(location_name){
+            var request = {
+                location:{lat:37.773972, lng: -122.431297},
+                radius:50000,
+                name: location_name + " San Francisco"
+            }
         })
+    }
+    remove_markers(markers)
+    markers = []
+    var service = new google.maps.places.PlacesService(map);
+    requests.forEach(function(request){
+        service.radarSearch(request, 
+            function(results, status)
+            {
+                if (status == google.maps.places.PlacesServiceStatus.OK) 
+                {
+                    var lat = (results[0].geometry.location.lat())
+                    var lng = (results[0].geometry.location.lng())
+                    var marker = new google.maps.Marker({
+                        position: {lat: lat, lng:lng},
+                        title: value
+                    });
+                    markers.push(marker);
+                };
+
+            })
+    })
+    markers.forEach(function(marker){
+        marker.setMap(map)
+    })
+    map.setZoom(12)
 }
 
-
+var remove_markers = function(markers){
+    markers.forEach(function(marker){
+        marker.setMap(null)
+    })
+}
